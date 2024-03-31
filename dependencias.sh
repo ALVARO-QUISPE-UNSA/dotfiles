14f9f27ae3ac374a16b682c3adb32bf259af42f2
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-03-30 22:10:03 -0500

    Some of the extra tools

---
+++ dependencias.sh
@@ -0,0 +1,10 @@
+sudo apt install bat
+sudo apt install fzf
+sudo apt install cargo
+cargo install git-delta
+# WARNINGS 
+# se añade al path el cargo
+sudo apt-get install silversearcher-ag
+#interesante
+# curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+# Te descarga un paquete de git hub
