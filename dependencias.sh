a7920e6adb20d3144a756183bf848e00c5c56dd1
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-04-25 16:46:38 -0500

    I added dependencies to install my tools

--- dependencias.sh
+++ dependencias.sh
@@ -5,6 +5,17 @@ cargo install git-delta
 # WARNINGS 
 # se añade al path el cargo
 sudo apt-get install silversearcher-ag
+
+# intalar lo que es el diccionario para vim
+# dentro del directorio spell en vim
+wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.latin1.spl
+wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.latin1.sug
+wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.utf-8.spl
+wget --no-check-certificate https://ftp.vim.org/vim/runtime/spell/es.utf-8.sug
+
 #interesante
 # curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 # Te descarga un paquete de git hub
+
+# support
+# npm install -g neovim
